1ecbd78e66c486b807980a1645f576bf819a3fd9
eycanny <nancy.the.coder@gmail.com>
2022-03-25 19:36:01 -0700

    Created administer() method for class Exam; added docstrings to various methods; updated class Question's __repr__() method

--- oo.py
+++ oo.py
@@ -2,6 +2,8 @@ class Student():
     """Student data."""
 
     def __init__(self, first_name, last_name, address):
+        """Instantiate an object with a Student class."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
@@ -10,11 +12,15 @@ class Question():
     """Question data."""
 
     def __init__(self, question, correct_answer):
+        "Instantiate an object with a Question class."
+
         self.question = question
         self.correct_answer = correct_answer
 
 
     def ask_and_evaluate(self):
+        """Ask user for answer and evaluate answer."""
+
         user_answer = input(f"{self.question} > ")
         if user_answer == self.correct_answer:
             return True
@@ -23,28 +29,55 @@ class Question():
 
 
     def __repr__(self):
-        return f" Question: {self.question}\nAnswer:{self.correct_answer}"
+        return f"{self.question},{self.correct_answer}"
 
 class Exam():
     """Exam data."""
+    
     questions = []
 
     def __init__(self, name):
+        """Instantiate an object with class Exam attributes."""
+
         self.name = name
 
 
     def add_question(self, question):
+        """Add question to an exam's list of questions."""
+
         self.questions.append(question)
 
         return self.questions
 
 
+    def administer(self):
+        """Administer all questions of an exam."""
+
+        correct_answers = 0
+        i = 0
+
+        for question in self.questions[::1]:
+            answer = question.ask_and_evaluate()
+            if answer == True:
+                correct_answers += 1
+        
+        correct_percent = float(correct_answers / len(self.questions[::1]) * 100)
+
+        return f"You scored a {correct_percent:.2f}%!"
+
+
+
     def __repr__(self):
+        """Returns list of available questions in an exam."""
+
         return f"Question List: {self.questions}"
 
-set_q = Question(
-                'What is the method for adding an element to a set?',
-                '.add()'
-                )
-exam = Exam('midterm')
-print(exam.add_question(set_q))
+
+##Test##
+# exam = Exam('midterm')
+# set_q = Question('What is the method for adding an element to a set?', '.add()')
+# exam.add_question(set_q)
+# pwd_q = Question('What does pwd stand for?', 'print working directory')
+# exam.add_question(pwd_q)
+# list_q = Question('Python lists are mutable, iterable, and what?', 'ordered')
+# exam.add_question(list_q)
