befddb75126d820067bf28b7eaf2beb75ffa6672
eycanny <nancy.the.coder@gmail.com>
2022-03-25 18:27:57 -0700

    Created three classes: Student, Question, Exam

--- oo.py
+++ oo.py
@@ -1,1 +1,21 @@
-# Create your classes here
+class Student():
+    """Student data."""
+
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+class Question():
+    """Question data."""
+
+    def __init__(self, question, correct_answer):
+        self.question = question
+        self.correct_answer = correct_answer
+
+class Exam():
+    """Exam data."""
+
+    def __init__(self, name, questions)
+        self.name = name
+        self.questions = []
